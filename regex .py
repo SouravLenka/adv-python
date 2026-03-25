140fc841b77afca552ff2aaca327709a9d4c0a7d
SouravLenka <lenkasourav09@gmail.com>
2026-03-25 10:03:05 +0530

    regex update 2

--- regex .py
+++ regex .py
@@ -1,7 +1,18 @@
 import regex as re
-pattern=("python")
-match=re.search(pattern,"python is a programming language")
-if match:
-    print("pattern found")
-else:
-    print("pattern not found")
+q=int(input("enter the number to execute: "))
+
+if q==1:
+    pattern=("python")
+    search=re.search(pattern,"python is a programming language")
+    if search:
+        print("pattern found")
+    else:
+        print("pattern not found")
+
+elif q==2:
+    pattern="^a...s$"
+    result=re.match(pattern,"abyss")
+    print(result)
+    
+elif q==3:
+    string="hello 12 hi 89.Howdy 34"    
